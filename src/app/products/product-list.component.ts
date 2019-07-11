e074643e072132e928f9bf61293e32a3764f8de0
hassan elhamaky <hassan.elhamaky@yahoo.com>
2019-07-11 22:27:46 +0200

    customized filtering pipe, get and set, constructor and here we go! filter is working!

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,17 @@ export class ProductListComponent {
     imageWidth:number = 50;
     imageMargin : number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    _listFilter: string;
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+
+    }
+
+    filteredProducts : IProduct[];
     products: IProduct[] = [
         {
             "productId": 2,
@@ -36,8 +46,20 @@ export class ProductListComponent {
           }
     ];
 
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy : string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
     } 
+    
     
 }
