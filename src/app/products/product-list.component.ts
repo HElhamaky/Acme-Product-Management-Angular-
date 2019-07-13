823f04c6f461e8b5409ce225c8f1d58d7fadd699
hassan elhamaky <hassan.elhamaky@yahoo.com>
2019-07-13 19:41:52 +0200

    product service is added using angular injector

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from './product';
+import { ProductService } from "./product.service";
 
 @Component({
     selector:'pm-products',
@@ -23,32 +24,10 @@ export class ProductListComponent {
     }
 
     filteredProducts : IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          }
-    ];
+    products: IProduct[] = [];
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _ProductService : ProductService){
+    
     }
 
     onRatingClicked(message:string):void {
@@ -63,7 +42,12 @@ export class ProductListComponent {
 
     toggleImage(): void {
         this.showImage = !this.showImage;
-    } 
+    }
+
+    ngOnInit():void{
+        this.products = this._ProductService.getProducts();
+        this.filteredProducts = this.products;
+    }
     
     
 }
